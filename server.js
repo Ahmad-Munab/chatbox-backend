64f8fd15f0aa6055d8b348cf73347b7eb912d82f
Ahmad Munab <ahmadmunab22@gmail.com>
2023-01-13 20:45:46 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+PORT = 3500;
+DATABASE_URI = process.env.DATABASE_URI;
+const app = express();
+
+// Defining middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+app.use('/', require('./routes/root.js'));
+app.use('/register', require('./routes/registerRoute'));
+app.use('/login', require('./routes/loginRoute'));
+app.get('/users', require('./controllers/getAllUsers'));
+
+
+app.all("*", (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(__dirname + '/views/404.html')
+    } else if (req.accepts("json")) {
+        res.json({ message: "404 Not found" })
+    } else {
+        res.type("txt").send("404 Not found")
+    }
+})
+
+
+// Connecting to MongoDB
+mongoose.set('strictQuery', true)
+const connectDB = async () => {
+    try {
+        await mongoose.connect(DATABASE_URI)
+    } catch (err) {
+        console.log(err)
+    }
+}
+connectDB()
+
+// Starting server and MongoDB
+mongoose.connection.once("open", () => {
+    console.log("Connected to MongoDB")
+    app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`))
+
+})
+
+//Checking for err in MongoDB
+mongoose.connection.on("error", err => {
+    console.log(err)
+})
